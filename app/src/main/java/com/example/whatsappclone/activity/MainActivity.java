796599a6822d24507c441b787b208d9a0c6f6e9d
Lorrane Alves <Lorrane.dev@gmail.com>
2021-12-22 14:08:02 -0300

    Adicionado toolbar e configurado menu principal

--- app/src/main/java/com/example/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/activity/MainActivity.java
@@ -1,23 +1,72 @@
 package com.example.whatsappclone.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.whatsappclone.R;
+import com.example.whatsappclone.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Whatsapp");
         setSupportActionBar(toolbar);
 
+//        FragmentPagerItemAdapter adapter = FragmentPagerItemAdapter(
+//                getSupportFragmentManager(),
+//                FragmentPagerItems.with(this)
+//                .add()
+//                        .add()
+//                .create()
+//        );
 
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                deslogarUsuario();
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+        try {
+            auth.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
