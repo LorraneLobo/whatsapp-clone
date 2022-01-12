6db86a842a0edc6d30f7dfda5d3402ac6e05bdab
Lorrane Alves <Lorrane.dev@gmail.com>
2022-01-12 11:34:27 -0300

    Salvando mensagens no firebase

--- app/src/main/java/com/example/whatsappclone/activity/ChatActivity.java
+++ app/src/main/java/com/example/whatsappclone/activity/ChatActivity.java
@@ -4,27 +4,32 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
+import com.example.whatsappclone.config.ConfiguracaoFirebase;
 import com.example.whatsappclone.databinding.ActivityChatBinding;
+import com.example.whatsappclone.helper.Base64Custom;
+import com.example.whatsappclone.helper.UsuarioFirebase;
+import com.example.whatsappclone.model.Mensagem;
 import com.example.whatsappclone.model.Usuario;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 
 import com.example.whatsappclone.R;
+import com.google.firebase.database.DatabaseReference;
 
 public class ChatActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityChatBinding binding;
     private Usuario usuarioDestinatario;
+
+    //identificador usuarios remetente e destinatario
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,9 @@ public class ChatActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Configuracoes iniciais
+
+        //recupera dados do usuario remetente
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
 
         //Recuperar dados do usuário destinatario
         Bundle bundle = getIntent().getExtras();
@@ -57,7 +65,37 @@ public class ChatActivity extends AppCompatActivity {
             }else{
                 binding.circleImageFotoChat.setImageResource(R.drawable.padrao);
             }
+
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
         }
+
+    }
+
+    public void enviarMensagem(View v){
+        String textoMensagem = binding.content.editMensagem.getText().toString();
+
+        if (!textoMensagem.isEmpty()){
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+
+            //salvar mensagem para o remetente
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+
+            //Limpar texto
+            binding.content.editMensagem.setText("");
+        }
+
+    }
+
+    public void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg){
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+
+        mensagemRef.child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(msg);
 
     }
 
